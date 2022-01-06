42269ace136328c55a5c4af60cd1ab2cff315828
Axel <axelehrlich8@gmail.com>
2022-01-06 17:31:44 +0100

    add usestate for controlling item in my todo received container

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 
 const App = () => {
 
+  const initialValue = []
   const [value, setValue] = useState('')
+  const [todo, setTodo] = useState(initialValue)
 
   const onChange = (event) => {
     setValue(event.target.value);
   }
 
+  const onclickSendValue = () => {
+    value.push(initialValue)
+  }
 
 
 
@@ -16,11 +21,18 @@ const App = () => {
     <div className="App">
       <div className='todo_container'>
         <input onChange={onChange} value={value} type="text"></input>
-        <button>Envoyer</button>
+        <button onClick={onclickSendValue}>Envoyer</button>
       </div>
+
+
     <div className='main_main_container'>
       <div className='main_container_received'>
-          <div className='container_received'></div>
+        {todo.map((todoItem) => {
+          return <div key={todoItem} className='container_received'>{todoItem}</div>
+        })
+
+        }
+          
       </div>
     </div>
       
