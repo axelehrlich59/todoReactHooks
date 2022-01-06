647df529a3996df950b6bd8ff1e8a0ca7c242fb4
Axel <axelehrlich8@gmail.com>
2022-01-06 17:08:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='todo_container'>
+        <input onChange={onChange} value={value} type="text"></input>
+        <button>Envoyer</button>
+      </div>
+    <div className='main_main_container'>
+      <div className='main_container_received'>
+          <div className='container_received'></div>
+      </div>
+    </div>
+      
     </div>
   );
 }
