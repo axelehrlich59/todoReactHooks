b0448355004f73ca08afca8ee3d5c39dc3ab48c3
Axel <axelehrlich8@gmail.com>
2022-01-31 16:26:31 +0100

    controll limited characters in input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css'
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
 const App = () => {
   const initialValue = []
@@ -11,11 +12,19 @@ const App = () => {
   const [todo, setTodo] = useState(initialValue)
 
   const onChange = (event) => {
-    setValue(event.target.value)
+    const inputValue = event.target.value
+    setValue(inputValue)
+
+    const maxCharacters = 80
+    if(inputValue.length > maxCharacters) {
+      const inputLimited = inputValue.substr(0, maxCharacters)
+      setValue(inputLimited)
+    }
   }
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
+      //const todoElipsis = 
       setTodo([...todo, value])
       setValue('')
     }
@@ -25,6 +34,8 @@ const App = () => {
     const newTodos = todo.filter((_, index) => index !== todoIndex);
     setTodo(newTodos)
   }
+
+  
 
   return (
     <div className="App">
@@ -38,11 +49,12 @@ const App = () => {
         <div className='test'>
           {todo.map((todoItem, index) => {
             return  <> 
-            <div key={todoItem} className='container_received'>
-              {todoItem}
-              <FontAwesomeIcon icon={faTrash} className='trash-icon' onClick={() => deleteTodoItem(index)} />
-            </div> 
-              <hr className='hr'></hr>
+              <div key={todoItem} className='container_received'>
+                <input className='checkbox' type="checkbox"></input>
+                {todoItem}
+                <FontAwesomeIcon icon={faTrash} className='trash-icon' onClick={() => deleteTodoItem(index)} />
+              </div> 
+                <hr className='hr'></hr>
             </>
           })}
         </div>
